da0e031bf7b230add38cc70209365fe4bf666fba
Brandon Collins <brandoncollins@Brandons-iMac.local>
2020-05-30 11:01:20 -0400

    Updates

---
+++ precache-manifest.f65edd10ebf11a75065394616e5ebf12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34663877d20840d287e4ef7b938fa27c",
+    "url": "/react-youtube-clone/index.html"
+  },
+  {
+    "revision": "d3a2046fcf6c2c46e777",
+    "url": "/react-youtube-clone/static/js/2.78cc9f9c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-youtube-clone/static/js/2.78cc9f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53abe0456e709f9db793",
+    "url": "/react-youtube-clone/static/js/main.8d591b82.chunk.js"
+  },
+  {
+    "revision": "81536656446a26eda6b5",
+    "url": "/react-youtube-clone/static/js/runtime-main.b2bde6e8.js"
+  }
+]);
